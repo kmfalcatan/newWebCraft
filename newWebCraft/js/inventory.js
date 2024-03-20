8bcaf9a45c0e472107b7de66ad53590e9d6ca348
FirstName Falcatan <kmfalcatan2@gmail.com>
2024-03-21 03:12:35 +0800

    ajsdakjsbd

--- newWebCraft/js/inventory.js
+++ newWebCraft/js/inventory.js
@@ -33,3 +33,29 @@ function setting1(){
         popUp.style.display = 'none';
     }
 }
+
+function track(){
+    var container = document.querySelector('.trackButton');
+    var popUp = document.querySelector('.subTrackContainer');
+    var close = document.querySelector('.button3');
+
+    if(popUp.style.display === 'none'){
+        popUp.style.display = 'block';
+    } else if(popUp.style.display === 'block'){
+        popUp.style.display = 'none';
+    } else{
+        popUp.style.display = 'none';
+    }
+}
+
+function track1(){
+    var popUp = document.querySelector('.trackUnitContainer');
+
+    if(popUp.style.display === 'none'){
+        popUp.style.display = 'block';
+    } else if(popUp.style.display === 'block'){
+        popUp.style.display = 'none';
+    } else{
+        popUp.style.display = 'none';
+    }
+}
